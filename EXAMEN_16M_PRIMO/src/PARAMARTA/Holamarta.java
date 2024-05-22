3b070760365f15efc2d7ffde1db0dd7692a8d738
PC <PC@DESKTOP-V9GOQ3S>
2024-05-22 12:12:46 +0200

    a

---
+++ EXAMEN_16M_PRIMO/src/PARAMARTA/Holamarta.java
@@ -0,0 +1,13 @@
+package PARAMARTA;
+
+import java.util.Scanner;
+
+public class Holamarta {
+	public static void main(String[]args) {
+		Scanner ent=new Scanner(System.in);
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+		System.out.println("Introduce un número para averiguar si es primo o no:");
+		int numero=Integer.parseInt(ent.nextLine());
+	}
+
+}
